848e47cd04f6d890f108f6f6c280303c6fef9baf
许瑞 <xurui@threegne.com>
2023-01-16 15:22:00 +0800

    add should return runner when call effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -39,4 +39,6 @@ let activeEffect;
 export const effect = (fn: any) => {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+
+  return _effect.run.bind(_effect);
 };
