76ecc2ab2f2ae97a739e0cf246b722d3b7dad040
许瑞 <xurui@threegne.com>
2023-01-16 17:37:58 +0800

    add effect -> stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  onStop?: () => void;
+  active = true;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -7,7 +12,22 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      clearupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
 }
+
+export const clearupEffect = (effect) => {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+};
 
 let targetMap = new Map();
 export const track = (target, key) => {
@@ -22,8 +42,9 @@ export const track = (target, key) => {
     dep = new Set();
     depsMap.set(key, dep);
   }
-
+  if (!activeEffect) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 };
 
 export const trigger = (target, key) => {
@@ -39,11 +60,19 @@ export const trigger = (target, key) => {
   }
 };
 
+export const stop = (runner) => {
+  runner.effect.stop();
+};
+
 let activeEffect;
 export const effect = (fn: any, options: any = {}) => {
   const _effect = new ReactiveEffect(fn, options.scheduler);
-
+  // _effect.onStop = options.onStop;
+  // Object.assign(_effect, options);
+  extend(_effect, options);
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
 };
